b1ddb7760036c5346bf9f0c22b2653c5e40ba7fb
atxrenegade <harleighabel@gmail.com>
2017-07-20 22:32:51 -0500

    add program fuctions and basic execution

---
+++ CLI_Gratitude.c
@@ -0,0 +1,40 @@
+/*
+Harleigh Abel
+July 20 2017
+CLI Gratitude App
+
+This program a an application of personal interest to 
+practice my C programming skills.  This program records 
+a user's daily gratitude entries and stores them to be 
+printed or reviewed later. It requires a username and password
+to access stored entries. */
+
+#include <stdio.h>
+
+int main(void)
+{
+	greeting();
+	menu();
+	exit();
+
+	return 0;
+}
+
+
+*************************************************
+*************************************************
+**************  PROGRAM FUNCTIONS  **************
+*************************************************
+*************************************************
+
+// greeting(); 
+// login();
+// new_user();
+// menu();
+// save();
+// printToScreen();
+// sendToPrinter();
+// newEntry();
+// print();
+// reviewEntry();
+// exit();
