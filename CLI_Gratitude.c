16b15d6b109558eed026d39e29f58e1dcad482d9
Harleigh Abel <harleighabel@gmail.com>
2017-07-22 01:17:16 -0500

    add greeting() and prototypes

--- CLI_Gratitude.c
+++ CLI_Gratitude.c
@@ -11,6 +11,10 @@ to access stored entries. */
 
 #include <stdio.h>
 
+void greeting();
+void newUser();
+void login();
+
 int main(void)
 {
 	greeting();
@@ -21,13 +25,38 @@ int main(void)
 }
 
 
-*************************************************
+/*************************************************
 *************************************************
 **************  PROGRAM FUNCTIONS  **************
 *************************************************
-*************************************************
+*************************************************/
 
-// greeting(); 
+// 
+void greeting(); 
+printf("Welcome to Daily Gratitude Application");
+printf("Can you please tell me are you a new or exisiting user?(N = new)(E = Existing user)): ");
+scanf(" %c", &user);
+user = toupper(user); // validate user data, convert lower to upper, remove extra characters
+if (user == 'N')
+{
+	newUser();
+}
+else if (user == 'E')
+{
+	login();
+}
+else
+{
+	printf("Your reply was not understood please choose (N for new user or E for Existing user)")
+}
+// validate user data, convert lower to upper, remove extra characters
+
+
+
+
+
+
+
 // login();
 // new_user();
 // menu();
