6c324340a456a09ca696d006cf896f25d7f86f14
Harleigh Abel <harleighabel@gmail.com>
2017-08-25 23:49:37 -0500

    document login()

--- CLI_Gratitude.c
+++ CLI_Gratitude.c
@@ -142,25 +142,33 @@ void test()
 void login()
 
 {
+  // allocate memory to create and store variables for username and password
   char *username = (char*) malloc(30*sizeof(char));
   char *password = (char*) malloc(30*sizeof(char));
+  // prompt user to input username and store input	
   printf("Please enter your username: ");
   scanf(" %s", username);
+  // prompt user to input password and store input	
   printf("Please enter your password: ");
   scanf(" %s", password);	
   // create hash of username and password
   // open file storing saved usernames and saved passwords
   // search database for username and password combo
-  if if(strcmp(username, "user") == 0)&& if(strcmp(password, "default") == 0)
+  // compare user input username and password combo to stored hash key and value	
+  if(strcmp(username, "user") == 0)&& if(strcmp(password, "default") == 0)
   {
+    // confirm login if a match	  
     printf("Login successful");
     return;
   }
   else 
   {
+    // error message if no match 
+    // return to login prompts	  
     printf("Im sorry you username and/or password was incorrect!"\n);
     login();
   }
+    // free allocated memory for user input username and password variables 	
     free(username);
     free(password);
 }
